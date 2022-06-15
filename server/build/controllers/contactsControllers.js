06ffc9c46c4432864ad9bed0cf5724b351267fa6
callmeanto <15-11196@usb.ve>
2022-06-15 21:00:16 +0200

    WIP

--- server/build/controllers/contactsControllers.js
+++ server/build/controllers/contactsControllers.js
@@ -4,25 +4,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = __importDefault(require("../database"));
-const database_2 = __importDefault(require("../database"));
 class ContactsController {
-    getContacts(req, res) {
-        database_2.default.query('DESCRIBE contacts');
-        res.json('contacts');
+    async getContacts(req, res) {
+        const contacts = await database_1.default.query('SELECT * FROM contacts');
+        res.json(contacts);
     }
-    getContact(req, res) {
-        database_2.default.query('DESCRIBE contacts');
-        res.json('contacts');
+    async getContact(req, res) {
+        const { id } = req.params;
+        const contacts = await database_1.default.query('SELECT * FROM contacts WHERE id = ?', [id]);
+        if (contacts.length > 0) {
+            return res.json(contacts[0]);
+        }
+        res.status(404).json({ text: "The contact does not exist" });
     }
     async create(req, res) {
         await database_1.default.query('INSERT INTO contacts set ?', [req.body]);
         res.json({ message: 'Contact saved' });
     }
-    delete(req, res) {
-        res.json({ text: 'Deleting a game' });
+    async delete(req, res) {
+        const { id } = req.params;
+        await database_1.default.query('DELETE FROM contacts WHERE id = ?', [id]);
+        res.json({ message: 'Contact deleted' });
     }
-    update(req, res) {
-        res.json({ text: 'Updating game' + req.params.id });
+    async update(req, res) {
+        const { id } = req.params;
+        await database_1.default.query('UPDATE contacts set ? WHERE id = ?', [req.body, id]);
+        res.json({ text: 'Updating contact ' + req.params.id });
     }
 }
 const contactsController = new ContactsController();
--- server/src/controllers/contactsControllers.ts
+++ server/src/controllers/contactsControllers.ts
@@ -1,6 +1,5 @@
 import {Request, Response} from 'express';
 import pool from '../database';
-import db from '../database';
 
 
 class ContactsController {
