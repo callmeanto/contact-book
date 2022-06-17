64f4951fba2efd25702f79e6f4e034db53895a1c
callmeanto <15-11196@usb.ve>
2022-06-17 09:57:03 +0200

    changing max length params

--- database/database.sql
+++ database/database.sql
@@ -1,13 +1,13 @@
-CREATE DATABASE ng_contacts_db;
+CREATE DATABASE contacts_db;
 
-USE ng_contacts_db;
+USE contacts_db;
 
 CREATE TABLE contacts(
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(9),
-    last_name VARCHAR(12),
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     email VARCHAR(30),
-    phone VARCHAR(12)
+    phone VARCHAR(30)
 );
 
 DESCRIBE contacts;
