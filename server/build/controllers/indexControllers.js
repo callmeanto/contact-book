1c4236bad2b4fa153d496b396d855ed18d6e8929
callmeanto <15-11196@usb.ve>
2022-06-21 15:33:06 +0200

    removing build/ folder

--- server/build/controllers/indexControllers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class IndexController {
-    index(req, res) {
-        res.json({ text: 'API is /api/contacts' });
-    }
-}
-const indexController = new IndexController();
-exports.default = indexController;
